c5646a52bf4d54784c7c6edcbfc330abb59271de
Francis Eugene Casibu <projectnevz@gmail.com>
2026-07-11 22:20:08 +0800

    make it work (done) make it right (later) make it fast (later)

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,58 @@ use x11rb::protocol::Event;
 use x11rb::protocol::xproto::*;
 use x11rb::rust_connection::RustConnection;
 
+use thiserror::Error;
+
 const KEY_ENTER: u8 = 36;
+const MAX_CLIENTS: usize = 256;
+
+#[derive(Error, Debug)]
+pub enum WMError {
+    #[error("connection error")]
+    ConnectionError(#[from] x11rb::errors::ConnectionError),
+
+    #[error("reply error")]
+    ReplyError(#[from] x11rb::errors::ReplyError),
+
+    #[error("standard error")]
+    StandardError(#[from] std::io::Error),
+}
+
+#[derive(PartialEq, Debug)]
+struct Client {
+    id: Window,
+    x: i16,
+    y: i16,
+    width: u16,
+    height: u16,
+}
+
+impl Client {
+    fn new(id: Window, x: i16, y: i16, width: u16, height: u16) -> Self {
+        Self {
+            id,
+            x,
+            y,
+            width,
+            height,
+        }
+    }
+
+    fn from_geometry(id: Window, geom: &GetGeometryReply) -> Self {
+        Self::new(id, geom.x, geom.y, geom.width, geom.height)
+    }
+}
 
 struct WM {
     // Testing purposes for now
     display_target: String,
 
     conn: RustConnection,
+    screen: Screen,
     root: u32,
-    clients: Vec<Window>,
+    clients: Vec<Client>,
     focused: Window,
 }
-
-const MAX_CLIENTS: usize = 256;
 
 impl WM {
     fn new() -> Self {
@@ -30,15 +69,21 @@ impl WM {
         let change = ChangeWindowAttributesAux::new()
             .event_mask(EventMask::SUBSTRUCTURE_REDIRECT | EventMask::SUBSTRUCTURE_NOTIFY);
 
-        let res = conn.change_window_attributes(root, &change).unwrap().check();
+        let res = conn
+            .change_window_attributes(root, &change)
+            .unwrap()
+            .check();
 
-        if let Err(ReplyError::X11Error(error)) = res && error.error_kind == ErrorKind::Access {
+        if let Err(ReplyError::X11Error(error)) = res
+            && error.error_kind == ErrorKind::Access
+        {
             eprintln!("Another wm is already running.");
             std::process::exit(1);
         }
 
         Self {
             display_target,
+            screen: screen.clone(),
             conn,
             root,
             clients: Vec::with_capacity(MAX_CLIENTS),
@@ -46,21 +91,62 @@ impl WM {
         }
     }
 
-    fn handle_events(&mut self, event: Event) -> Result<(), Box<dyn std::error::Error>> {
+    fn relayout(&mut self) -> Result<(), WMError> {
+        let len = self.clients.len() as u16;
+
+        for (index, client) in self.clients.iter_mut().enumerate() {
+            let new_width = self.screen.width_in_pixels / len;
+            client.width = new_width;
+            client.x = (new_width * index as u16) as i16;
+
+            self.conn.configure_window(
+                client.id,
+                &ConfigureWindowAux::new()
+                    .width(client.width as u32)
+                    .height(client.height as u32)
+                    .x(client.x as i32),
+            )?;
+
+            self.conn.change_window_attributes(
+                client.id,
+                &ChangeWindowAttributesAux::new().event_mask(EventMask::ENTER_WINDOW),
+            )?;
+            self.conn.map_window(client.id)?;
+        }
+
+        Ok(())
+    }
+
+    fn handle_events(&mut self, event: Event) -> Result<(), WMError> {
         match event {
             Event::MapRequest(event) => {
-                if self.clients.len() < MAX_CLIENTS {
-                    self.clients.push(event.window);
+                let client_id = event.window;
 
-                    self.conn.change_window_attributes(
-                        event.window,
-                        &ChangeWindowAttributesAux::new().event_mask(EventMask::ENTER_WINDOW),
+                if self.clients.len() < MAX_CLIENTS {
+                    self.conn.configure_window(
+                        client_id,
+                        &ConfigureWindowAux::new()
+                            .width(self.screen.width_in_pixels as u32)
+                            .height(self.screen.height_in_pixels as u32),
                     )?;
-                    self.conn.map_window(event.window)?;
+
+                    let geom = self.conn.get_geometry(client_id)?.reply()?;
+                    self.clients.push(Client::from_geometry(client_id, &geom));
+                    self.relayout()?;
+
+                    for client in &self.clients {
+                        tracing::info!("{client:?}");
+                    }
+
+                    self.conn.set_input_focus(
+                        InputFocus::PARENT,
+                        self.clients.last().map_or(self.root, |c| c.id),
+                        x11rb::CURRENT_TIME,
+                    )?;
                 } else {
                     eprintln!("There can only be {MAX_CLIENTS} clients at once.");
 
-                    self.conn.destroy_window(event.window)?;
+                    self.conn.destroy_window(client_id)?;
                 }
 
                 self.conn.flush().ok();
@@ -70,36 +156,49 @@ impl WM {
 
             Event::KeyPress(_) => {
                 // Just test opening any window for now
-                std::process::Command::new("alacritty")
+                std::process::Command::new("firefox")
                     .env("DISPLAY", &self.display_target)
                     .spawn()?;
             }
 
             Event::EnterNotify(event) => {
-                self.conn
-                    .set_input_focus(InputFocus::PARENT, event.event, x11rb::CURRENT_TIME)?;
                 self.focused = event.event;
             }
 
-            Event::UnmapNotify(_) => {
-                todo!("IMPORTANT");
+            Event::UnmapNotify(event) => {
+                self.conn
+                    .set_input_focus(InputFocus::PARENT, self.root, x11rb::CURRENT_TIME)?;
+                self.focused = self.root;
+                tracing::info!(
+                    "Window = {}, Event = {}, from_configure = {}",
+                    event.window,
+                    event.event,
+                    event.from_configure
+                );
             }
 
             Event::DestroyNotify(event) => {
-                if let Some(index) = self.clients.iter().position(|&win| win == event.window) {
+                if let Some(index) = self
+                    .clients
+                    .iter()
+                    .position(|client| client.id == event.window)
+                {
                     self.clients.remove(index);
 
                     let win = if self.clients.is_empty() {
                         self.root
                     } else {
-                        *self
-                            .clients
+                        self.clients
                             .get(index.saturating_sub(1))
                             .expect("should not be out of bounds")
+                            .id
                     };
 
+                    self.relayout()?;
+
                     self.focused = win;
-                    self.conn.set_input_focus(InputFocus::PARENT, win, x11rb::CURRENT_TIME)?;
+                    self.conn
+                        .set_input_focus(InputFocus::PARENT, win, x11rb::CURRENT_TIME)?;
                 }
 
                 tracing::info!("Clients = {}", self.clients.len());
@@ -117,7 +216,9 @@ impl WM {
 fn init_tracing() {
     use tracing_subscriber::prelude::*;
 
-    tracing_subscriber::registry().with(tracing_subscriber::fmt::layer()).init();
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -125,20 +226,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut wm = WM::new();
 
-    wm.conn.grab_key(
-        true,
-        wm.root,
-        ModMask::CONTROL,
-        KEY_ENTER,
-        GrabMode::ASYNC,
-        GrabMode::ASYNC,
-    ).expect("should be able to grab key");
+    wm.conn
+        .grab_key(
+            true,
+            wm.root,
+            ModMask::CONTROL,
+            KEY_ENTER,
+            GrabMode::ASYNC,
+            GrabMode::ASYNC,
+        )
+        .expect("should be able to grab key");
 
     loop {
         wm.conn.flush().ok();
 
-        let event = wm.conn.wait_for_event().expect("shkjkjkjkould receive event");
-        // TODO(fcasibu): handle errors
-        wm.handle_events(event).unwrap();
+        let event = wm.conn.wait_for_event().unwrap();
+        let mut event_option = Some(event);
+        while let Some(event) = event_option {
+            // TODO(fcasibu): handle errors
+            wm.handle_events(event).unwrap();
+            event_option = wm.conn.poll_for_event().unwrap();
+        }
     }
 }
